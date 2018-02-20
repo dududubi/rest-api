4b36067a183222ecc56c59deb62bda2bffbc66e9
dududubi <dududubi@naver.com>
2018-02-20 14:51:02 +0900

    test

--- main.go
+++ main.go
@@ -30,8 +30,6 @@ func main() {
     router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
     router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
     log.Fatal(http.ListenAndServe(":8000", router))
-	
-	
 }
 
 func GetPeople(w http.ResponseWriter, r *http.Request) {
